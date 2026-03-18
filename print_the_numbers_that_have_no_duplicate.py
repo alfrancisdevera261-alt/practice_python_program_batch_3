cc20a0afc62d03906d5c2a1aa55d3458780f706b
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-18 15:18:55 +0800

    added another for loop for the checking of the duplicated numbers

--- print_the_numbers_that_have_no_duplicate.py
+++ print_the_numbers_that_have_no_duplicate.py
@@ -1,5 +1,10 @@
 number_list = []
 for i in range(0,10):
-    numbers = int(input(f"Eneter number {i+1}: "))
+    numbers = int(input(f"Enter number {i+1}: "))
     number_list.append(numbers)
-print(number_list)
+
+result = []
+for number_count in number_list:
+    if number_list.count(number_count) == 1:
+        result.append(number_count)
+print(result)
